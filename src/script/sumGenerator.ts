a277ad5ebb455f9849c6d73776d11965e1ec6d84
Dan Corder <dev@dancorder.com>
2015-05-30 17:09:35 +0100

    Add choice of whether sums can include carrying

--- src/script/sumGenerator.ts
+++ src/script/sumGenerator.ts
@@ -2,23 +2,25 @@
 
 module Sums {
     export class SumGenerator {
-        getSums(numberOfSums: number, maxDigits: number)  {
+        getSums(numberOfSums: number, maxDigits: number, allowCarrying: boolean)  {
             var sums : Sum[] = [];
             
             for (var i = 0; i < numberOfSums; i++) {
-                sums.push(this.getSum(maxDigits));
+                sums.push(this.getSum(maxDigits, allowCarrying));
             }
             
             return sums;
         }
     
-        private getSum(maxDigits: number) : Sum {
+        private getSum(maxDigits: number, allowCarrying: boolean) : Sum {
             var operand1: number = 0;
             var operand2: number = 0;
             
             for (var i = 0; i < maxDigits; i++) {
                 var digit1: number = this.getRandomIntBetween(0, 9);
-                var digit2: number = this.getRandomIntBetween(0, 9-digit1);
+                
+                var upperBoundForDigit2 = allowCarrying ? 9 : 9-digit1
+                var digit2: number = this.getRandomIntBetween(0, upperBoundForDigit2);
                 
                 operand1 += (digit1 * Math.pow(10, i));
                 operand2 += (digit2 * Math.pow(10, i));
--- src/script/viewModel.ts
+++ src/script/viewModel.ts
@@ -8,6 +8,7 @@ module LongArithmatic {
             this.sumGenerator = sumGenerator;
             this.numberOfSums(12);
             this.maxDigits(10);
+            this.allowCarrying(true);
             
             this.refreshSums();
         }
@@ -15,9 +16,10 @@ module LongArithmatic {
         public sums = ko.observableArray<Sums.Sum>();
         public numberOfSums = ko.observable<number>();
         public maxDigits = ko.observable<number>();
+        public allowCarrying = ko.observable<boolean>();
         
         public refreshSums() {
-            this.sums(this.sumGenerator.getSums(this.numberOfSums(), this.maxDigits()));
+            this.sums(this.sumGenerator.getSums(this.numberOfSums(), this.maxDigits(), this.allowCarrying()));
         }
     }
 }
