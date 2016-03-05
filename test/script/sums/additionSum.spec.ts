2c8ace7a0b14d636c0ca5e90d2833f7ce3d101f7
Dan Corder <dev@dancorder.com>
2016-03-05 08:39:04 +0000

    Improver addition tests

--- test/script/sums/additionSum.spec.ts
+++ test/script/sums/additionSum.spec.ts
@@ -18,60 +18,178 @@ describe("additionSum", function() {
         expect(underTest.operand2.length).toEqual(6);
     });
     
-    it("generates sums without carrying", function() {
-        // Digits are random. Assume that 100 is enough to get an example failure
-        var underTest = new Sums.AdditionSum(100, 100, false);
+    describe("without carrying", function() {
+        describe("with equal operand lengths", function() {
+            it("generates sums without carrying", function() {
+                // Digits are random. Assume that 100 iterations is enough to get an example failure
+                for (var i = 0; i < 100; i++) {
+                    var underTest = new Sums.AdditionSum(2, 2, false);
+                    expect(underTest.operand1[0] + underTest.operand2[0]).toBeLessThan(10);
+                    expect(underTest.operand1[1] + underTest.operand2[1]).toBeLessThan(10);
+                }
+            });
+            
+            it("generates sums with correct leading digits", function() {
+                // Digits are random. Assume that 100 is enough to get an example failure
+                for (var i = 0; i < 100; i++) {
+                    var underTest = new Sums.AdditionSum(1, 1, false);
+                    expect(underTest.operand1[0]).not.toBe(0);
+                    expect(underTest.operand2[0]).not.toBe(0);
+                }
+            });
+        });
         
-        for (var i = 0; i < 100; i++) {
-            expect(underTest.operand1[i] + underTest.operand2[i]).toBeLessThan(10);
-        }
-    });
-    
-    it("generates sums without carrying when operand 2 is shorter", function() {
-        // Digits are random. Assume that 100 is enough to get an example failure
-        for (var i = 0; i < 100; i++) {
-            var underTest = new Sums.AdditionSum(2, 1, false);
-            expect(underTest.operand1[0] + underTest.operand2[0]).toBeLessThan(10);
-        }
-    });
-    
-    it("generates sums without carrying with correct leading digits", function() {
-        // Digits are random. Assume that 100 is enough to get an example failure
-        for (var i = 0; i < 100; i++) {
-            var underTest = new Sums.AdditionSum(1, 1, false);
-            expect(underTest.operand1[0] + underTest.operand2[0]).toBeLessThan(10);
-        }
-    });
-    
-    it("generates sums with carrying", function() {
-        // Digits are random. Assume that 100 is enough to get an example pass
-        var underTest = new Sums.AdditionSum(100, 100, true);
-        
-        var passCaseFound = false;
-        for (var i = 0; i < 100; i++) {
-            if (underTest.operand1[i] + underTest.operand2[i] >= 10) {
-                passCaseFound = true;
-                break;
+        describe("where operand 1 is shorter", function() {
+            it("generates sums without carrying", function() {
+            // Digits are random. Assume that 100 is enough to get an example failure
+            for (var i = 0; i < 100; i++) {
+                var underTest = new Sums.AdditionSum(2, 3, false);
+                expect(underTest.operand1[0] + underTest.operand2[0]).toBeLessThan(10);
+                expect(underTest.operand1[1] + underTest.operand2[1]).toBeLessThan(10);
             }
-        }
-        expect(passCaseFound).toBeTruthy();
+            });
+            
+            it("generates sums with correct leading digits", function() {
+                // Digits are random. Assume that 100 is enough to get an example failure
+                for (var i = 0; i < 100; i++) {
+                    var underTest = new Sums.AdditionSum(2, 3, false);
+                    expect(underTest.operand1[1]).not.toBe(0);
+                    expect(underTest.operand2[2]).not.toBe(0);
+                }
+            });
+        });
+        
+        describe("where operand 2 is shorter", function() {
+            it("generates sums without carrying", function() {
+            // Digits are random. Assume that 100 is enough to get an example failure
+            for (var i = 0; i < 100; i++) {
+                var underTest = new Sums.AdditionSum(3, 2, false);
+                expect(underTest.operand1[0] + underTest.operand2[0]).toBeLessThan(10);
+                expect(underTest.operand1[1] + underTest.operand2[1]).toBeLessThan(10);
+            }
+            });
+            
+            it("generates sums with correct leading digits", function() {
+                // Digits are random. Assume that 100 is enough to get an example failure
+                for (var i = 0; i < 100; i++) {
+                    var underTest = new Sums.AdditionSum(3, 2, false);
+                    expect(underTest.operand1[2]).not.toBe(0);
+                    expect(underTest.operand2[1]).not.toBe(0);
+                }
+            });
+        });
     });
     
-    it("generates sums without carrying without a leading 0", function() {
-        // Digits are random. Assume that 100 is enough to get a failure
-        for (var i = 0; i < 100; i++) {
-            var underTest = new Sums.AdditionSum(1, 1, false);
-            expect(underTest.operand1[0]).not.toBe(0);
-            expect(underTest.operand2[0]).not.toBe(0);
-        }
-    });
-    
-    it("generates sums with carrying without a leading 0", function() {
-        // Digits are random. Assume that 100 is enough to get a failure
-        for (var i = 0; i < 100; i++) {
-            var underTest = new Sums.AdditionSum(1, 1, true);
-            expect(underTest.operand1[0]).not.toBe(0);
-            expect(underTest.operand2[0]).not.toBe(0);
-        }
+    describe("with carrying", function() {
+        describe("with equal operand lengths", function() {
+            it("generates sums with carrying on leading digits", function() {
+                // Digits are random. Assume that 100 is enough to get an example pass
+                var passCaseFound = false;
+                for (var i = 0; i < 100; i++) {
+                    var underTest = new Sums.AdditionSum(1, 1, true);
+                    if (underTest.operand1[0] + underTest.operand2[0] >= 10) {
+                        passCaseFound = true;
+                        break;
+                    }
+                }
+                expect(passCaseFound).toBeTruthy();
+            });
+            
+            it("generates sums with carrying on trailing digits", function() {
+                // Digits are random. Assume that 100 is enough to get an example pass
+                var passCaseFound = false;
+                for (var i = 0; i < 100; i++) {
+                    var underTest = new Sums.AdditionSum(2, 2, true);
+                    if (underTest.operand1[0] + underTest.operand2[0] >= 10) {
+                        passCaseFound = true;
+                        break;
+                    }
+                }
+                expect(passCaseFound).toBeTruthy();
+            });
+            
+            it("generates sums without a leading 0", function() {
+                // Digits are random. Assume that 100 is enough to get a failure
+                for (var i = 0; i < 100; i++) {
+                    var underTest = new Sums.AdditionSum(1, 1, true);
+                    expect(underTest.operand1[0]).not.toBe(0);
+                    expect(underTest.operand2[0]).not.toBe(0);
+                }
+            });
+        });
+        
+        describe("where operand 1 is shorter", function() {
+            it("generates sums with carrying on leading digits", function() {
+                // Digits are random. Assume that 100 is enough to get an example pass
+                var passCaseFound = false;
+                for (var i = 0; i < 100; i++) {
+                    var underTest = new Sums.AdditionSum(1, 2, true);
+                    if (underTest.operand1[0] + underTest.operand2[0] >= 10) {
+                        passCaseFound = true;
+                        break;
+                    }
+                }
+                expect(passCaseFound).toBeTruthy();
+            });
+            
+            it("generates sums with carrying on trailing digits", function() {
+                // Digits are random. Assume that 100 is enough to get an example pass
+                var passCaseFound = false;
+                for (var i = 0; i < 100; i++) {
+                    var underTest = new Sums.AdditionSum(2, 3, true);
+                    if (underTest.operand1[0] + underTest.operand2[0] >= 10) {
+                        passCaseFound = true;
+                        break;
+                    }
+                }
+                expect(passCaseFound).toBeTruthy();
+            });
+            
+            it("generates sums without a leading 0", function() {
+                // Digits are random. Assume that 100 is enough to get a failure
+                for (var i = 0; i < 100; i++) {
+                    var underTest = new Sums.AdditionSum(1, 2, true);
+                    expect(underTest.operand1[0]).not.toBe(0);
+                    expect(underTest.operand2[1]).not.toBe(0);
+                }
+            });
+        });
+        
+        describe("where operand 2 is shorter", function() {
+            it("generates sums with carrying on leading digits", function() {
+                // Digits are random. Assume that 100 is enough to get an example pass
+                var passCaseFound = false;
+                for (var i = 0; i < 100; i++) {
+                    var underTest = new Sums.AdditionSum(2, 1, true);
+                    if (underTest.operand1[0] + underTest.operand2[0] >= 10) {
+                        passCaseFound = true;
+                        break;
+                    }
+                }
+                expect(passCaseFound).toBeTruthy();
+            });
+            
+            it("generates sums with carrying on trailing digits", function() {
+                // Digits are random. Assume that 100 is enough to get an example pass
+                var passCaseFound = false;
+                for (var i = 0; i < 100; i++) {
+                    var underTest = new Sums.AdditionSum(3, 2, true);
+                    if (underTest.operand1[0] + underTest.operand2[0] >= 10) {
+                        passCaseFound = true;
+                        break;
+                    }
+                }
+                expect(passCaseFound).toBeTruthy();
+            });
+            
+            it("generates sums without a leading 0", function() {
+                // Digits are random. Assume that 100 is enough to get a failure
+                for (var i = 0; i < 100; i++) {
+                    var underTest = new Sums.AdditionSum(2, 1, true);
+                    expect(underTest.operand1[1]).not.toBe(0);
+                    expect(underTest.operand2[0]).not.toBe(0);
+                }
+            });
+        });
     });
 });
