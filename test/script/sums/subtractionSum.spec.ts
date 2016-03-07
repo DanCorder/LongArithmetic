8b87a0f806a71c5b9d3d045fa7cdf54719e9d3b8
Dan Corder <dev@dancorder.com>
2016-03-07 18:24:32 +0000

    Add more subtraction sum tests

--- test/script/sums/subtractionSum.spec.ts
+++ test/script/sums/subtractionSum.spec.ts
@@ -18,24 +18,6 @@ describe("subtractionSum", function() {
         expect(underTest.operand2.length).toEqual(3);
     });
 
-    it("generates sums without carrying", function() {
-        // Digits are random. Assume that 100 is enough to get an example failure
-        const underTest = new Sums.SubtractionSum(100, 100, false);
-
-        for (let i = 0; i < 100; i++) {
-            expect(underTest.operand1[i]).not.toBeLessThan(underTest.operand2[i]);
-        }
-    });
-
-    it("generates sums without carrying when operand 2 is shorter", function() {
-        // Digits are random. Assume that 100 is enough to get an example failure
-        for (let i = 0; i < 100; i++) {
-            const underTest = new Sums.SubtractionSum(2, 1, false);
-
-            expect(underTest.operand1[0]).not.toBeLessThan(underTest.operand2[0]);
-        }
-    });
-
     it("truncates the second operand to match the first", function() {
         let underTest = new Sums.SubtractionSum(5, 8, false);
 
@@ -48,44 +30,137 @@ describe("subtractionSum", function() {
         expect(underTest.operand2.length).toEqual(6);
     });
 
-    it("generates sums with carrying", function() {
-        // Digits are random. Assume that 100 is enough to get an example pass
-        const underTest = new Sums.SubtractionSum(100, 100, true);
+    describe("without carrying", function() {
+        describe("with equal operand lengths", function() {
+            it("generates sums without carrying", function() {
+                // Digits are random. Assume that 100 is enough to get an example failure
+                for (let i = 0; i < 100; i++) {
+                    const underTest = new Sums.SubtractionSum(2, 2, false);
+                    expect(underTest.operand1[0]).not.toBeLessThan(underTest.operand2[0]);
+                    expect(underTest.operand1[1]).not.toBeLessThan(underTest.operand2[1]);
+                }
+            });
 
-        let passCaseFound = false;
-        for (let i = 0; i < 100; i++) {
-            if (underTest.operand1[i] - underTest.operand2[i] < 0) {
-                passCaseFound = true;
-                break;
+            it("generates sums with correct leading digits", function() {
+                // Digits are random. Assume that 100 is enough to get an example failure
+                for (let i = 0; i < 100; i++) {
+                    const underTest = new Sums.SubtractionSum(1, 1, false);
+                    expect(underTest.operand1[0]).not.toBe(0);
+                    expect(underTest.operand2[0]).not.toBe(0);
+                }
+            });
+        });
+
+        describe("where operand 1 is shorter", function() {
+            it("generates sums without carrying", function() {
+            // Digits are random. Assume that 100 is enough to get an example failure
+            for (let i = 0; i < 100; i++) {
+                const underTest = new Sums.SubtractionSum(2, 3, false);
+                expect(underTest.operand1[0]).not.toBeLessThan(underTest.operand2[0]);
+                expect(underTest.operand1[1]).not.toBeLessThan(underTest.operand2[1]);
             }
-        }
-        expect(passCaseFound).toBeTruthy();
+            });
+
+            it("generates sums with correct leading digits", function() {
+                // Digits are random. Assume that 100 is enough to get an example failure
+                for (let i = 0; i < 100; i++) {
+                    const underTest = new Sums.SubtractionSum(2, 3, false);
+                    expect(underTest.operand1[1]).not.toBe(0);
+                    expect(underTest.operand2[2]).not.toBe(0);
+                }
+            });
+        });
+
+        describe("where operand 2 is shorter", function() {
+            it("generates sums without carrying", function() {
+            // Digits are random. Assume that 100 is enough to get an example failure
+            for (let i = 0; i < 100; i++) {
+                const underTest = new Sums.SubtractionSum(3, 2, false);
+                expect(underTest.operand1[0]).not.toBeLessThan(underTest.operand2[0]);
+                expect(underTest.operand1[1]).not.toBeLessThan(underTest.operand2[1]);
+            }
+            });
+
+            it("generates sums with correct leading digits", function() {
+                // Digits are random. Assume that 100 is enough to get an example failure
+                for (let i = 0; i < 100; i++) {
+                    const underTest = new Sums.SubtractionSum(3, 2, false);
+                    expect(underTest.operand1[2]).not.toBe(0);
+                    expect(underTest.operand2[1]).not.toBe(0);
+                }
+            });
+        });
     });
 
-    it("generates sums with carrying with positive answers", function() {
-        // Digits are random. Assume that 100 is enough to get an example failure
-        for (let i = 0; i < 100; i++) {
-            const underTest = new Sums.SubtractionSum(2, 2, true);
-            expect(10 * underTest.operand1[1] + underTest.operand1[0])
-                .not.toBeLessThan(10 * underTest.operand2[1] + underTest.operand2[0]);
-        }
-    });
+    describe("with carrying", function() {
+        describe("with equal operand lengths", function() {
+            it("generates sums with positive answers", function() {
+                // Digits are random. Assume that 100 is enough to get an example failure
+                for (let i = 0; i < 100; i++) {
+                    const underTest = new Sums.SubtractionSum(2, 2, true);
+                    expect(10 * underTest.operand1[1] + underTest.operand1[0])
+                        .not.toBeLessThan(10 * underTest.operand2[1] + underTest.operand2[0]);
+                }
+            });
 
-    it("generates sums without carrying without a leading 0", function() {
-        // Digits are random. Assume that 100 is enough to get a failure
-        for (let i = 0; i < 100; i++) {
-            const underTest = new Sums.SubtractionSum(1, 1, false);
-            expect(underTest.operand1[0]).not.toBe(0);
-            expect(underTest.operand2[0]).not.toBe(0);
-        }
-    });
+            it("generates sums with carrying on trailing digits", function() {
+                // Digits are random. Assume that 100 is enough to get an example pass
+                let passCaseFound = false;
+                for (let i = 0; i < 100; i++) {
+                    const underTest = new Sums.SubtractionSum(2, 2, true);
+                    if (underTest.operand1[0] - underTest.operand2[0] < 0) {
+                        passCaseFound = true;
+                        break;
+                    }
+                }
+                expect(passCaseFound).toBeTruthy();
+            });
 
-    it("generates sums with carrying without a leading 0", function() {
-        // Digits are random. Assume that 100 is enough to get a failure
-        for (let i = 0; i < 100; i++) {
-            const underTest = new Sums.SubtractionSum(1, 1, true);
-            expect(underTest.operand1[0]).not.toBe(0);
-            expect(underTest.operand2[0]).not.toBe(0);
-        }
+            it("generates sums without a leading 0", function() {
+                // Digits are random. Assume that 100 is enough to get a failure
+                for (let i = 0; i < 100; i++) {
+                    const underTest = new Sums.SubtractionSum(1, 1, true);
+                    expect(underTest.operand1[0]).not.toBe(0);
+                    expect(underTest.operand2[0]).not.toBe(0);
+                }
+            });
+        });
+
+        describe("where operand 2 is shorter", function() {
+            it("generates sums with carrying on leading digits", function() {
+                // Digits are random. Assume that 100 is enough to get an example pass
+                let passCaseFound = false;
+                for (let i = 0; i < 100; i++) {
+                    const underTest = new Sums.SubtractionSum(3, 2, true);
+                    if (underTest.operand1[1] - underTest.operand2[1] < 0) {
+                        passCaseFound = true;
+                        break;
+                    }
+                }
+                expect(passCaseFound).toBeTruthy();
+            });
+
+            it("generates sums with carrying on trailing digits", function() {
+                // Digits are random. Assume that 100 is enough to get an example pass
+                let passCaseFound = false;
+                for (let i = 0; i < 100; i++) {
+                    const underTest = new Sums.SubtractionSum(3, 2, true);
+                    if (underTest.operand1[0] - underTest.operand2[0] < 0) {
+                        passCaseFound = true;
+                        break;
+                    }
+                }
+                expect(passCaseFound).toBeTruthy();
+            });
+
+            it("generates sums without a leading 0", function() {
+                // Digits are random. Assume that 100 is enough to get a failure
+                for (let i = 0; i < 100; i++) {
+                    const underTest = new Sums.SubtractionSum(2, 1, true);
+                    expect(underTest.operand1[1]).not.toBe(0);
+                    expect(underTest.operand2[0]).not.toBe(0);
+                }
+            });
+        });
     });
 });
