39d27eade6fe261fa646c87e70b85534bf0b5531
Dan Corder <dev@dancorder.com>
2016-05-09 08:35:29 +0100

    Tidy ViewModel constructor

--- src/script/viewModel.ts
+++ src/script/viewModel.ts
@@ -4,33 +4,21 @@
 namespace LongArithmatic {
     export class ViewModel {
         constructor(private problemSetGenerator: Sums.ProblemSetGenerator) {
-            this.numberOfSums(12);
-            this.digitsTop(10);
-            this.digitsBottom(10);
-            this.allowCarrying(true);
-            this.sumType(Sums.Operator.Add);
-            this.allowedSumTypes([ Sums.Operator.Add, Sums.Operator.Subtract ]);
-
-            this.ordering(Sums.Ordering.AscendingOperand1);
-            this.allowedOrderings([Sums.Ordering.AscendingOperand1, Sums.Ordering.DescendingOperand1, Sums.Ordering.Random]);
-
-            this.showSolutions(false);
-
             this.generateRandomSums();
         }
 
         public sums = ko.observableArray<Sums.RandomSum>();
-        public numberOfSums = ko.observable<number>();
-        public digitsTop = ko.observable<number>();
-        public digitsBottom = ko.observable<number>();
-        public allowCarrying = ko.observable<boolean>();
-        public sumType = ko.observable<Sums.Operator>();
-        public allowedSumTypes = ko.observableArray<Sums.Operator>();
+        public numberOfSums = ko.observable<number>(12);
+        public digitsTop = ko.observable<number>(10);
+        public digitsBottom = ko.observable<number>(10);
+        public allowCarrying = ko.observable<boolean>(true);
+        public sumType = ko.observable<Sums.Operator>(Sums.Operator.Add);
+        public allowedSumTypes = ko.observableArray<Sums.Operator>([ Sums.Operator.Add, Sums.Operator.Subtract ]);
 
-        public ordering = ko.observable<Sums.Ordering>();
-        public allowedOrderings = ko.observableArray<Sums.Ordering>();
+        public ordering = ko.observable<Sums.Ordering>(Sums.Ordering.AscendingOperand1);
+        public allowedOrderings = ko.observableArray<Sums.Ordering>([Sums.Ordering.AscendingOperand1, Sums.Ordering.DescendingOperand1, Sums.Ordering.Random]);
 
-        public showSolutions = ko.observable<boolean>();
+        public showSolutions = ko.observable<boolean>(false);
 
         public generateRandomSums() {
             this.sums(this.problemSetGenerator.getRandomSums(this.numberOfSums(), this.sumType(), this.digitsTop(), this.digitsBottom(), this.allowCarrying()));
